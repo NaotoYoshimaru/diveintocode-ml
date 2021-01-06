a2e1da29c4670af66d381aef2de949fedac13132
NaotoYoshimaru <yoshimaru@goodround.jp>
2021-01-06 09:32:56 +0900

    week1 課題提出

---
+++ week1.py
@@ -0,0 +1,111 @@
+#問題1
+THICKNESS = 0.00008
+folded_thickness = THICKNESS*2**43
+print("厚さ： {}メートル".format(folded_thickness))
+
+#問題2
+answer1 = folded_thickness/1000
+#print(answer1)
+
+answer2=answer1 / 10000
+print("厚さ： {:.2f}万キロメートル".format(answer2))
+
+moon_distance = 38.40
+if answer2 > moon_distance:
+  print("月まで届きました。いい夢を！")
+else:
+  print("月まで届きませんでした。残念！")
+
+#問題3
+THICKNESS = 0.00008
+S = a = 2
+n = 43
+
+for i in range(n-1):
+    S *= a
+    answer3 = THICKNESS * S
+    answer4 = answer3/10000000
+print("厚さ : {:.2f}万キロメートル".format(answer4))
+
+#問題4
+
+# ベキ乗**使う方
+import time
+start = time.time()
+#####
+THICKNESS = 0.00008
+folded_thickness = THICKNESS*2**43
+#print("厚さ： {}メートル".format(folded_thickness))
+#####
+elapsed_time = time.time() - start
+print("ベキ乗**を使う方法")
+print("time : {}[s]".format(elapsed_time))
+
+# for文使う方
+import time
+start = time.time()
+#####
+THICKNESS = 0.00008
+S = a =  2
+n = 43
+
+for i in range(n-1):
+    S *= a
+    answer = THICKNESS * S
+#print("厚さ: {}メートル".format(answer))
+#####
+elapsed_time = time.time() - start
+print("for文を使う方法")
+print("time : {}[s]".format(elapsed_time))
+
+
+#問題5
+box = [0.00008, 0.00016]
+THICKNESS = 0.00008
+S = a =  2
+n = 43
+
+for i in range(n-1):
+    S *= a
+    answer = THICKNESS * S
+    box.append(answer)
+    print(box)
+#print(answer)
+print(len(box))
+
+#問題6
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.title("thickness of folded paper")
+plt.xlabel("number of folds")
+plt.ylabel("thickness[m]")
+plt.plot(box)
+plt.show()
+'''37回目あたりから急激に厚みが増す'''
+
+#問題7
+#パターン1(線の色を緑にする)
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.title("thickness of folded paper")
+plt.xlabel("number of folds")
+plt.ylabel("thickness[m]")
+plt.plot(box, color="green")
+plt.show()
+#パターン2(ラインの太さを太くする)
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.title("thickness of folded paper")
+plt.xlabel("number of folds")
+plt.ylabel("thickness[m]")
+plt.plot(box, linewidth=4)
+plt.show()
+#パターン3(マーカーをつける,グリッドを入れる)
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.title("thickness of folded paper")
+plt.xlabel("number of folds")
+plt.ylabel("thickness[m]")
+plt.plot(box, marker="o")
+plt.grid()
+plt.show()
